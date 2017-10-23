2793691250b6a7db5cd2285a9050c44077d1340a
Morgan Fudge <mfudge92@gmail.com>
2017-10-23 15:04:51 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -19,7 +19,7 @@ def turn(board)
 end
 
 def move(board, index, value = "X")
-  if(index.to_i && valid_move?(board, index))
+  if(valid_move?(board, index))
     board[index] = value
     return true
   end
